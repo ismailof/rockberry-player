63b918b6dcc218a3138c08e3a71f12f2d638a4d3
ismailof <ismailof@github.com>
2016-12-09 20:22:33 +0100

    updated debug.py

--- rockberry_player/debug.py
+++ rockberry_player/debug.py
@@ -1,17 +1,28 @@
 from functools import wraps
 
 
+# TODO: Change prints for debugs ?
 def debug_function(_function_):
     @wraps(_function_)
     def wrapper(*args, **kwargs):
-        print('[CALL] %r, args: %r, kwargs: %s' % (_function_.__name__,
-                                                   args,
-                                                   kwargs))
-        return_value = _function_(*args, **kwargs)
+        print('[CALL] {}, args: {}, kwargs: {}'.format(
+            _function_.__name__, args, kwargs)
+        )
+
+        try:
+            return_value = _function_(*args, **kwargs)
+        except Exception as ex:
+            print('[EXCEPTION] {} \n {}'.format(
+                _function_.__name__, ex)
+            )
+            return None
+
         if return_value is not None:
-            print('[RETURN] %r returned %r %r' % (_function_.__name__,
-                                                  return_value,
-                                                  type(return_value)))
+            print('[RETURN] {} returned {} {}'.format(
+                _function_.__name__,
+                return_value,
+                type(return_value))
+            )
             return return_value
 
     return wrapper
